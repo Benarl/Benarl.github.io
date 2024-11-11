1c115f06588d5cc3cb5cd99c4560e5280fcb2548
Benoit ARLIAUD <b.arliaud@nodya-group.com>
2024-11-11 07:12:37 +0100

    start from portfolio template

--- shader_test/default.glsl
+++
@@ -1,9 +0,0 @@
-void main() 
-{ 
-    vec2 uv = gl_FragCoord.xy / iResolution.xy;
-    vec2 uvm=iMouse/iResolution.xy;
-    
-
-    //gl_FragColor = texture2D(iChannel0, uv);
-     gl_FragColor = vec4(uv*uvm, 0.5, 1);
-}
--- shader_test/utils.js
+++
@@ -1,9 +0,0 @@
-
-export function loadElement(tag)
-{
-    var element=document.querySelector("#"+tag);
-    if (!element) {
-        throw new Error('Unable to find element with id: ' + tag);
-    }
-    return element;
-}
